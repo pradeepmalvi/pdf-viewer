13a46dd6d51bf481163f933f1a6317d20315db13
pradeepmalvi <p.malviyaaa@gmail.com>
2021-11-02 11:04:53 +0530

    initial Commit

---
+++ src/components/PdfViewer.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const PdfViewer = () => {
+  const [pdf, setPdf] = useState(null);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  function removeTextLayerOffset() {
+    const textLayers = document.querySelectorAll(
+      ".react-pdf__Page__textContent"
+    );
+    textLayers.forEach((layer) => {
+      const { style } = layer;
+      style.color = "unset";
+    });
+
+    const canvasLayer = document.querySelectorAll(".react-pdf__Page__canvas");
+    canvasLayer.forEach((layer) => {
+      const { style } = layer;
+      style.visibility = "hidden";
+    });
+  }
+
+  <Page />;
+
+  const uploadPdf = (e) => {
+    console.log(e.target.files[0]);
+    setPdf(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <input type="file" onChange={uploadPdf} />
+      <div className="pdf-viewer" style={{ width: "50%", margin: "0 auto" }}>
+        {pdf && (
+          <>
+            <div>
+              <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page
+                  onLoadSuccess={removeTextLayerOffset}
+                  pageNumber={pageNumber}
+                />
+              </Document>
+              <p>
+                Page {pageNumber} of {numPages}
+              </p>
+            </div>{" "}
+            <button onClick={() => setPageNumber(pageNumber + 1)}>
+              Next page
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default PdfViewer;
